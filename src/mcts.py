67e812b3f80db2fbae7599f328207e76bdb03f67
Dongyeongkim <dongyeongkim33@gmail.com>
2023-01-16 12:23:00 +0000

    few updates

--- src/mcts.py
+++ src/mcts.py
@@ -7,9 +7,9 @@ from preprocess import hashing_state
 class Node(object):
     def __init__(self, state, parent):
         super().__init__()
-        self.state = state
-        self.parent = parent
-        self.address = hashing_state(state)
+        self.state = state # this state should be original state.
+        self.parent = parent # Node
+        self.address = hashing_state(state) 
         self.childs = []
         self.visits = 1
         self.wins = 0
@@ -17,9 +17,19 @@ class Node(object):
 
 
 class MCTS(object):
-    def __init__(self, stm, ):
+    def __init__(self, stm, initial_state=None):
         super().__init__()
-        self.stm = stm
+        device_name = 'cpu'
+        if torch.cuda.is_available():
+            device_name = "cuda:0"
+        
+        self.stm = stm.to(torch.device(device_name))
+        self.env = puoribor.PuoriborEnv()
+        
+        if initial_state != None:
+            self.startingNode = Node(initial_state, None)
+        else:
+            self.startingNode = Node(self.env.initialize_state(), None)
 
     def UCT(self, ):
         pass
