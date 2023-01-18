85c1d846e100cfe4534f353b9210836ce2f23690
Dongyeongkim <dongyeongkim33@gmail.com>
2023-01-18 15:23:48 +0000

    dataset part has been updated: cache will be updated also

--- src/dataset.py
+++ src/dataset.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 from numba import jit
+from tqdm import tqdm
 from preprocess import preprocessor
 from fights.envs.puoribor import PuoriborEnv
 
-
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
 
 @jit(forceobj=True)
 def remapping_action(x):
@@ -48,13 +50,37 @@ def generate_puribor(lines):
 
 def data(path):
     file_list = os.listdir(path)
+    xdata = []; ydata = []
+    for fil in tqdm(file_list):
+        dlist = generate_puribor(open(path+fil).readlines())
+        for i, data in enumerate(dlist):
+            xdata.append(data[0])
+            ydata.append(data[1])
+    return xdata, ydata
 
-    for file in file_list:
-        dlist = generate_puribor(open(filepath+file).readlines())
+class PuriborDataset(Dataset):
+    def __init__(self, filepath="../data/"):
+
+        npz = "cache.npz"
+        cpath = "../cache/"
         
+        if not os.path.exists(cpath):
+            os.makedirs(cpath)
+        if os.path.exists(cpath+npz):
+            db = np.load(cpath+npz, allow_pickle=True)
+            self.x_data = db["xdata"]
+            self.y_data = db["ydata"]
+        else:
+            self.x_data, self.y_data = data(path=filepath)
+            np.savez_compressed(cpath+npz, xdata=np.array(self.x_data), ydata=np.array(self.y_data))
         
-
-
+    def __len__(self):
+        return len(self.x_data)
+    
+    def __getitem__(self, idx):
+        x = self.x_data[idx]
+        y = self.y_data[idx]
+        return list(x), list(y)
 
 if __name__ == '__main__':
-    data('../data/') 
+    PuriborDataset()
